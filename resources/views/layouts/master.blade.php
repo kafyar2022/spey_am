656e08ffc53e65f4b01f7bf19bda792914a4e003
kafyar2022 <kafyar2022@gmail.com>
2022-06-13 17:00:18 +0500

    google and yandex metrix added

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -50,12 +50,47 @@
   @yield('content')
   @include('layouts.footer')
   <!-- JQuery 3.6  -->
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"
-    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
   {{-- Owl carousel --}}
   <script src="{{ asset('js/owl.carousel.min.js') }}"></script>
   {{-- App Scripts --}}
   <script src="{{ mix('js/app.js') }}"></script>
+  <!-- Yandex.Metrika counter -->
+  <script type="text/javascript">
+    (function(m, e, t, r, i, k, a) {
+      m[i] = m[i]
+
+      function() {
+        (m[i].a = m[i].a[]).push(arguments)
+      };
+      m[i].l = 1 * new Date();
+      k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+    })
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+    ym(48786833, "init", {
+      clickmap: true,
+      trackLinks: true,
+      accurateTrackBounce: true,
+      webvisor: true
+    });
+  </script>
+  <noscript>
+    <div><img src="https://mc.yandex.ru/watch/48786833" style="position:absolute; left:-9999px;" alt="" /></div>
+  </noscript>
+  <!-- /Yandex.Metrika counter -->
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-123986695-30"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'UA-123986695-30');
+  </script>
 </body>
 
 </html>
